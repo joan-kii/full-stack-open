e53d537cb4ceeb0bd5450cda8a80eb42218bd620
joan-kii <joan.sb@hotmail.com>
2023-07-16 06:05:39 +0200

    add eslint

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,43 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
+const url = process.env.MONGODB_URI;
 
-const url = process.env.MONGODB_URI
-
-console.log('Connecting to database')
+console.log('Connecting to database');
 
 mongoose.connect(url)
-  .then(_ => {
-    console.log('Connected to MongoDB')
+  .then((_result) => {
+    console.log('Connected to MongoDB');
   })
-  .catch(err => {
-    console.log('Error connecting to MongoDB', err.message)
-  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB', err.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3, 
-    required: true
+    minLength: 3,
+    required: true,
   },
   number: {
     type: String,
     minLength: 8,
     validate: {
-      validator: (number) =>  /^\d{2,3}[-]\d+$/.test(number),
-      message: number => `${number.value} is not a valid phone number!`
-    }, 
-    required: true
-  }
-})
+      validator: (number) => /^\d{2,3}[-]\d+$/.test(number),
+      message: (number) => `${number.value} is not a valid phone number!`,
+    },
+    required: true,
+  },
+});
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+  transform: (_document, returnedObject) => {
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+    returnedObject.id = returnedObject._id.toString();
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+    delete returnedObject._id;
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
