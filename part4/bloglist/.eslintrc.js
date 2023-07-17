3d2b452bde29647ba9007c20ccdb55803b40b6b7
joan-kii <joan.sb@hotmail.com>
2023-07-17 05:12:49 +0200

    add part4

---
+++ part4/bloglist/.eslintrc.js
@@ -0,0 +1,42 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always',
+    ],
+    'arrow-spacing': [
+      'error', { before: true, after: true },
+    ],
+    'linebreak-style': ['error', 'windows'],
+    'no-console': 'off',
+    'no-unused-vars': ['error',
+      { destructuredArrayIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    'consistent-return': 'error',
+    'import/no-extraneous-dependencies': [
+      'error', { dependencies: false }],
+  },
+};
