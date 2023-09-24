3de81b57be45491209e5513738f11cf3f3a28093
joan-kii <joan.sb@hotmail.com>
2023-09-24 07:08:28 +0200

    add exercises 9.8 and 9.9

---
+++ part9/patientor-backend/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.get('/api/patients', (_req, res) => {
+  res.send([]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
