2ea656ef7b0277b40e2f0a054f1876e85cad4e81
joan-kii <joan.sb@hotmail.com>
2023-11-10 17:34:24 +0100

    add exercise 10.20

--- part10/rate-repository-app/src/components/SingleRepository/RepositoryReview.jsx
+++ part10/rate-repository-app/src/components/SingleRepository/RepositoryReview.jsx
@@ -6,22 +6,47 @@ import theme from '../../theme';
 
 const styles = StyleSheet.create({
   reviewContainer: {
+    padding: 10,
     flexDirection: 'row',
     backgroundColor: 'white'
   },
+  ratingContainer: {
+    width: '25%',
+    alignItems: 'center'
+  },
   rating: {
-    color: theme.colors.primary
-  }
+    width: 40,
+    height: 40,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+    borderRadius: 20,
+    fontWeight: theme.fontWeights.bold,
+    color: theme.colors.primary,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  username: {
+    fontWeight: theme.fontWeights.bold
+  },
+  textReview: {
+    paddingTop: 5,
+    width: '85%'
+  },
 });
 
 const RepositoryReview = ({ review }) => {
   return (
     <View style={styles.reviewContainer}>
-      <Text>{review.node.rating}</Text>
+      <View style={styles.ratingContainer}>
+        <Text style={styles.rating}>{review.node.rating}</Text>
+      </View>
       <View>
-        <Text>{review.node.username}</Text>
-        <Text>{review.node.createdAt}</Text>
-        <Text>{review.node.text}</Text>
+        <Text style={styles.username}>{review.node.user.username}</Text>
+        <Text>{new Date(review.node.createdAt).toLocaleDateString()}</Text>
+        <View style={styles.textReview}>
+          <Text>{review.node.text}</Text>
+        </View>
       </View>
     </View>
   );
