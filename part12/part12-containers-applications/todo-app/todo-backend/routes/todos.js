11a873db847b536d131e926b4884dc6f9753aa88
joan-kii <joan.sb@hotmail.com>
2023-12-09 07:06:53 +0100

    add exercise 12.7

--- part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,13 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  req.todo.done = req.body.done;
+  res.send(req.todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
--- part12/part12-containers-applications/todo-app/todo-backend/util/config.js
+++ part12/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = process.env.MONGO_URL || undefined
+const MONGO_URL = 'mongodb://the_username:the_password@localhost:3456/the_database' // process.env.MONGO_URL || undefined
 const REDIS_URL = process.env.REDIS_URL || undefined
 
 module.exports = {
