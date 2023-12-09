61b33da6cd77929befda9909a3b9ac992e023c0b
joan-kii <joan.sb@hotmail.com>
2023-12-09 08:01:33 +0100

    add exercise 12.8

--- part12/part12-containers-applications/todo-app/todo-backend/util/config.js
+++ part12/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -1,5 +1,5 @@
 const MONGO_URL = 'mongodb://the_username:the_password@localhost:3456/the_database' // process.env.MONGO_URL || undefined
-const REDIS_URL = process.env.REDIS_URL || undefined
+const REDIS_URL = '//localhost:6378' // process.env.REDIS_URL || undefined
 
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
