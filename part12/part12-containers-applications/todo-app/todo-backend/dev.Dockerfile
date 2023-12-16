fc0eed52824acd198fb39e022c3c701221e161df
joan-kii <joan.sb@hotmail.com>
2023-12-16 04:57:33 +0100

    add exercise 12.16

---
+++ part12/part12-containers-applications/todo-app/todo-backend/dev.Dockerfile
@@ -0,0 +1,10 @@
+FROM node:16
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN npm install
+
+USER node
+CMD npm run dev
--- part12/part12-containers-applications/todo-app/todo-backend/util/config.js
+++ part12/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -1,5 +1,5 @@
-const MONGO_URL = 'mongodb://the_username:the_password@localhost:3456/the_database' // process.env.MONGO_URL || undefined
-const REDIS_URL = 'redis://localhost:6379' // process.env.REDIS_URL || undefined
+const MONGO_URL = 'mongodb://the_username:the_password@mongo:27017/the_database' // process.env.MONGO_URL || undefined
+const REDIS_URL = 'redis://redis:6379' // process.env.REDIS_URL || undefined
 
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
